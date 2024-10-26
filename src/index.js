59660d95b2d7f30ce92cfd73c1ecd3e4f0135e90
Alesia <lesiakuharchuk@gmail.com>
2024-10-26 23:43:03 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 // index.js
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'; 
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import './styles/common.css' 
+import './styles/common.css';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container); // Создаем корневой узел с использованием createRoot
+
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App /> {/* Оборачиваем App в BrowserRouter */}
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
